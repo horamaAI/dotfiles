a0abb5d34f01feb703f56553b69a0b3aa6cd4343
myfirstdebianpc <rurayi@yahoo.fr>
2024-07-21 02:09:56 +0200

    DOTF000: draft plan, ongoing structure

--- profiles/basic/basic.sh
+++ profiles/basic/basic.sh
@@ -12,7 +12,7 @@ essentials=(
 
 # step 1. set up directories structure
 # fetch basic environment variables, after that some basic environment variables should be loaded and can be used
-source $DOTFILES_DIR/configs/basic/basic.env
+source $DOTFILES_DIR/configs-shared/basic/basic.env
 # load utils
 source $PROFILE_CONFIGS_DIR/bin/functions/misc.sh
 # create main folders structure
