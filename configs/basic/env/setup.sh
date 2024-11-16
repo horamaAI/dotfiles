57568438bad2d7129e3982fff33e6bbe4282e483
myfirstdebianpc <rurayi@yahoo.fr>
2024-11-16 23:41:36 +0100

    DOTF000: draft plan, ongoing, configuring containerized environment for dotfiles testing

--- configs/basic/env/setup.sh
+++ configs/basic/env/setup.sh
@@ -4,5 +4,8 @@ COMMENT=\#*
 
 # complete with environment variables from the specific installation
 echo "
+# use sequence 'jk' as <ESC> for zsh-vi-mode
+# otherwise it's annoying and can't use some shell keybindings
+export ZVM_VI_ESCAPE_BINDKEY=jk
 export DOTFILES=$FINAL_DOTFILES_DIR
 " >> $DOTFILES_TRGT_DIR/.zshrc_user
