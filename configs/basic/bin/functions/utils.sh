1c63c47317fdd7a41ad60ddf8fedf42a1da8bacf
myfirstdebianpc <rurayi@yahoo.fr>
2025-03-17 22:26:04 +0100

    DOTF000: draft plan, ongoing, configuring containerized environment for dotfiles testing

--- configs/basic/bin/functions/utils.sh
+++ configs/basic/bin/functions/utils.sh
@@ -16,6 +16,10 @@ execute_install_command() {
     # ~(@kv)~: parameter expansion zsh style
     installed_pkgs[$msg]=$(echo "$command_to_run" | sed -n "s/^.*${msg} //p")
     # return string form of associative array to be used by calling function
+    for apair in "${!installed_pkgs[@]}"
+    do
+      echo "tests in execute: [content](key: value): ($key: $value)"
+    done
     echo "${installed_pkgs[@]@K}" # expand associative array as string with ~@K~ parameter (might not work the same on all shells, for example zsh)
   fi
 }
