050f3cec3c4bad7a0bc8beb9c8c4a2c1b7b1c1b5
myfirstdebianpc <rurayi@yahoo.fr>
2025-03-23 20:04:38 +0100

    DOTF000: draft plan, ongoing, configuring containerized environment for dotfiles testing

--- configs/basic/bin/functions/utils.sh
+++ configs/basic/bin/functions/utils.sh
@@ -9,50 +9,49 @@ make_dir() {
 execute_install_command() {
   local msg=$1
   local command_to_run="$2"
-  echo "[$msg] attempting to run command: '$command_to_run'"
-  # associative array to store installed packages
-  # key: command run to install packages (variable $msg), value: space separated name of packages
-  # -n to name ref the associative array passed as argument
+  # 3rd argument: associative array that stores installed packages:
+  # - key: command run to install packages (variable $msg); - value: space separated name of packages
+  # "-n" used to name ref the associative array passed as argument
   declare -n installed_pkgs=$3
+  echo "[$msg] attempting to run command: '$command_to_run'"
   if [[ -n "$command_to_run" ]]; then
-    #eval "$command_to_run"
-    # ~(@kv)~: parameter expansion zsh style
-    # in the install command crop out: "install", and any option ("-someoption")
-    # => basically just keep the packages names
+    eval "$command_to_run"
+    # needed to keep trace of installed packages:
+    # in the install command crop out: "install", and any option ("-someoption"), i.e.: basically just keep the packages names (space separated)
     installed_pkgs[$msg]+=$(echo "$command_to_run" | sed -n "s/^.*${msg} \(install \)\?\(-[a-zA-Z] \)\?//p")
-    # return string form of associative array to be used by calling function
+    #old solution (instead of passing argument as nameref):`echo "${installed_pkgs[@]@K}" # expand associative array as string with ~@K~ parameter (might not work the same on all shells, for example zsh)`
     #for akey in "${!installed_pkgs[@]}"
     #do
     #  echo "tests in execute: [content](key: value): (${akey}: ${installed_pkgs[${akey}]})"
     #done
-    #old solution:`echo "${installed_pkgs[@]@K}" # expand associative array as string with ~@K~ parameter (might not work the same on all shells, for example zsh)`
-    echo "${installed_pkgs[@]@K}"
+    echo "${installed_pkgs[@]@K}" # display as info log the aliased associative array of installed packages
   fi
 }
 
 # validate that model satisfy schema
 # $1 is the schema, and $2 the data
+# eg: "pajv -s $BASIC_CONFIGS_DIR/schemas/cmds.yaml -d $yamlcommandfile"
 validate_model_against_schema() {
   [[ $(pajv -s $1 -d $2 | grep ' valid') ]] && echo "schema validates the data model" || echo "[WARN] schema does NOT validate the data model, please double check the data model"
 }
 
-# read "command_entries" (as defined in schemas/cmds.yaml) from yaml file
-# returns list of commands to execute, each with arguments (all items in pkgs concatenated)
+# reads 'command_entries' (schemas definition, eg: schemas/cmds.yaml) given bya yaml file
+# returns list of commands to execute, each with arguments the items (or "pkg"s) to install
 parse_command_entries_in_yaml() {
   local -n commands=$1
   local yamlcommandfile=$2
   local -n commandslist=$3
   #local commandslist=()
   echo "process file: $yamlcommandfile"
-  # pajv -s $BASIC_CONFIGS_DIR/schemas/cmds.yaml -d $yamlcommandfile
   echo "attempt to validate data model: $2"
   validate_model_against_schema $BASIC_CONFIGS_DIR/schemas/cmds.yaml $yamlcommandfile
+  # parse yaml files
   mapfile -d '' commands < <(yq '.command_entries[] | .command' $yamlcommandfile | tr -d '"' | tr '\n' '\0')
   for cmd in "${commands[@]}"
   do
     echo "fetch packages for command: $cmd"
     # not easy to use yq with environment variables, workaround with a pipe to jq
-    # sed is for trimming trailing space
+    # sed to trim trailing space
     pkgs=$(yq '.' $yamlcommandfile | jq --arg buff "$cmd" '.command_entries[] | select(.command == $buff) | .pkgs[]' | tr -d '"' | tr '\n' ' ' | sed 's/[ \t]*$//')
     #yq '.command_entries[] | .command' "$yamlcommandfile" | tr -d '"' | tr '\n' '\0'
     echo "verify string pkgs: $pkgs"
--- profiles/basic/basic.sh
+++ profiles/basic/basic.sh
@@ -7,7 +7,7 @@ cd "$DIR"
 
 # pwd
 
-# 0. set up directories structure, and other basic utils
+# 0. set up directories structure, and other basic utils:
 # fetch basic environment variables, after that basic environment variables should be loaded and ready to be used
 source $DOTFILES_ENV_DIR/configs/basic/env/basic.env
 # load utils
@@ -35,7 +35,7 @@ IFS="$buff_ifs"
 declare -A INSTALLED_APPS
 export INSTALLED_APPS
 
-# 1. always install first build-essential, and following tools
+# 1. always install first build-essential, and other pre-required tools
 apt_essentials=(
     build-essential # g++, make, etc.
     yq # to parse yaml files containing required installations, will install python3
@@ -61,7 +61,7 @@ npm_essentials=(
 # install prerequisites
 execute_install_command "apt" "sudo apt install ${apt_essentials[*]}" INSTALLED_APPS
 echo "next command (npm) is suspended (for now), does nothing, since npm doesn't check context at all, it just reinstall everything"
-execute_install_command "npm" "sudo npm install -g ${npm_essentials[*]}" INSTALLED_APPS
+#execute_install_command "npm" "sudo npm install -g ${npm_essentials[*]}" INSTALLED_APPS
 
 # buff_ifs="$IFS"
 # IFS=:
@@ -89,8 +89,8 @@ cp $BASIC_CONFIGS_DIR/vim/vimrc "$DOTFILES_TRGT_DIR/.vimrc"
 # 6. test that everything went well
 #bash $BASIC_CONFIGS_DIR/tests/test.bats
 
-# test: show content
-#for key value in "${(@kv)INSTALLED_APPS}"
+# test: show content 
+#for key value in "${(@kv)INSTALLED_APPS}" # where ~(@kv)~ is a parameter expansion zsh style, so might not work with other shells, bash for example
 for akey in "${!INSTALLED_APPS[@]}"
 do
   echo "[content INSTALLED_APPS](key: value): (${akey}: ${INSTALLED_APPS[${akey}]})"
